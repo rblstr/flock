2883c2e429a2d6ba94130780729041b2973412d1
Rob Lester <rblestr@gmail.com>
2013-11-06 13:47:48 +0000

    Added filterning of links for YouTube URLs

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ logging.getLogger().setLevel(logging.DEBUG)
 REDDIT_URL = 'www.reddit.com'
 USER_AGENT = 'flock/0.1 by /u/rblstr'
 
+def parseChildren(children):
+	links = []
+	for child in children:
+		if 'youtube' in child.get('url').lower() or 'youtu.be' in child.get('url').lower():
+			links.append(child)
+	return links
+
 def getLinks(subreddits):
 	connection = httplib.HTTPConnection(REDDIT_URL)
 	request_url = '/r/%s/top.json?t=week&limit=100' % '+'.join(subreddits)
@@ -21,7 +28,8 @@ def getLinks(subreddits):
 	body = response.read()
 	response_object = json.loads(body)
 	children = response_object['data']['children']
-	links = [child.get('data') for child in children]
+	children = [child.get('data') for child in children]
+	links = parseChildren(children)
 	return links
 
 @app.route('/', methods=['GET'])
