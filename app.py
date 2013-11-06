b58ce1a5baf900986662c7f96961111561fe4cbf
Rob Lester <rblestr@gmail.com>
2013-11-06 13:39:01 +0000

    Added getting subreddit top links Added connecting to Reddit and pulling top links from requested subreddits.

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
+import logging
+import json
+import httplib
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+REDDIT_URL = 'www.reddit.com'
+USER_AGENT = 'flock/0.1 by /u/rblstr'
+
+def getLinks(subreddits):
+	connection = httplib.HTTPConnection(REDDIT_URL)
+	request_url = '/r/%s/top.json?t=week&limit=100' % '+'.join(subreddits)
+	headers = {
+		'User-Agent' : USER_AGENT
+	}
+	connection.request('GET', request_url, headers=headers)
+	response = connection.getresponse()
+	body = response.read()
+	response_object = json.loads(body)
+	children = response_object['data']['children']
+	links = [child.get('data') for child in children]
+	return links
 
 @app.route('/', methods=['GET'])
 def front():
 	subreddits = request.args.get('subreddits')
 	if not subreddits:
 		return render_template('front.html')
-	subreddit_list = subreddits.split('+')
-	return ' '.join(subreddit_list)
+	subreddits = subreddits.split()
+	links = getLinks(subreddits)
+	return '<br>'.join([link.get("title") for link in links])
 
 if __name__ == '__main__':
 	app.run(debug=True)
