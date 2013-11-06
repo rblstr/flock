489b87ee999b50a8bbff28421a986821ceae5de7
Rob Lester <rblestr@gmail.com>
2013-11-06 14:00:17 +0000

    Added YouTube URL sanitizing and embed URL

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import urllib
+import urlparse
 import logging
 import json
 import httplib
@@ -10,10 +12,67 @@ logging.getLogger().setLevel(logging.DEBUG)
 REDDIT_URL = 'www.reddit.com'
 USER_AGENT = 'flock/0.1 by /u/rblstr'
 
+def getYouTubeURL(links):
+	youtube_ids = []
+	for entry in links:
+		v_id = urlparse.parse_qs(urlparse.urlparse(entry.get('url')).query).get("v")
+		if not v_id:
+			continue
+		v_id = v_id[0]
+		youtube_ids.append(v_id)
+
+	first_id = youtube_ids[0]
+	youtube_ids = youtube_ids[1:]
+	playlist = ",".join(youtube_ids)
+	playlist = unicode(playlist).encode('utf-8')
+
+	query = {
+		'autohide'			: 0,
+		'showinfo'			: 1,
+		'modestbranding'	: 1,
+		'rel'				: 0,
+		'playlist'			: playlist
+	}
+	query_string = urllib.urlencode(query)
+
+	youtube_url = "http://www.youtube.com/embed/%s?%s" % (first_id, query_string)
+	return youtube_url
+
+def sanitiseShortYouTubeURL(url):
+	# parse URL
+	parsed = urlparse.urlparse(url)
+	# get video id
+	video_id = parsed.path[1:]
+	if not video_id:
+		return None
+	# create new url
+	new_url = 'http://www.youtube.com/watch?v=%s' % video_id
+	return new_url
+
+def sanitiseYouTubeURL(url):
+	# parse query string
+	query_dict = urlparse.parse_qs(urlparse.urlparse(url).query)
+	# get "v" value
+	v = query_dict.get('v', None)
+	if not v:
+		return None
+	# create new url
+	new_url = 'http://www.youtube.com/watch?v=%s' % v[0]
+	return new_url
+
+def sanitiseURL(url):
+	if 'youtube' in url.lower():
+		return sanitiseYouTubeURL(url)
+	elif 'youtu.be' in url.lower():
+		return sanitiseShortYouTubeURL(url)
+	else:
+		return None
+
 def parseChildren(children):
 	links = []
 	for child in children:
-		if 'youtube' in child.get('url').lower() or 'youtu.be' in child.get('url').lower():
+		child['url'] = sanitiseURL(child.get('url'))
+		if child.get('url'):
 			links.append(child)
 	return links
 
@@ -39,7 +98,8 @@ def front():
 		return render_template('front.html')
 	subreddits = subreddits.split()
 	links = getLinks(subreddits)
-	return '<br>'.join([link.get("title") for link in links])
+	youtube_embed = '<iframe width="560" height="315" src="%s" frameborder="0" allowfullscreen></iframe>' % getYouTubeURL(links)
+	return youtube_embed
 
 if __name__ == '__main__':
 	app.run(debug=True)
