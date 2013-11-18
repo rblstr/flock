adcb4e16e27d3d6ad733a92aa9afc50b01de52f1
Rob Lester <rblestr@gmail.com>
2013-11-18 13:59:32 +0000

    Added mock python module for mocking functions in flock-tests Updated requirements.txt with mock

--- flock-tests.py
+++ flock-tests.py
@@ -1,50 +1,6 @@
 import unittest
+import mock
 import flock
-
-""" Override getRedditResponse() """
-original_getRedditResponse = flock.getRedditResponse
-
-def mock_getRedditResponse_no_response(subreddits):
-	return None
-
-def mock_getRedditResponse_no_youtube_links(subreddits):
-	return {
-		'data' : {
-			'children' : [
-				{
-					'data' : {
-						'url' : 'imgur.com'
-					}
-				}
-			]
-		}
-	}
-
-def mock_getRedditResponse_set_response(subreddits):
-	return {
-		'data' : {
-			'children' : [
-				{
-					'data' : {
-						'title' : 'Burial - Untrue (Full Album Mix)',
-						'url' : 'http://www.youtube.com/watch?v=wRpHf4X7FNM'
-					}
-				},
-				{
-					'data' : {
-						'title' : 'Sage The Gemini - Gas Pedal (Motez Edit)',
-						'url' : 'http://www.youtube.com/watch?v=cfLmW-dKtwg'
-					}
-				},
-				{
-					'data' : {
-						'title' : 'Koreless & Jacques Greene - Untitled',
-						'url' : 'http://www.youtube.com/watch?v=AY08MWIGYsk'
-					}
-				}
-			]
-		}
-	}
 
 
 class FrontpageTestCase(unittest.TestCase):
@@ -52,27 +8,66 @@ class FrontpageTestCase(unittest.TestCase):
 		flock.app.testing = True
 		self.app = flock.app.test_client()
 
-	def tearDown(self):
-		flock.getRedditResponse = original_getRedditResponse
-
 	def test_frontpage(self):
 		response = self.app.get('/', content_type='text/html')
 		self.assertEqual(response.status_code, 200)
 
 	def test_frontpage_subreddits_no_response(self):
-		flock.getRedditResponse = mock_getRedditResponse_no_response
+		flock.getRedditResponse = mock.MagicMock(name='getRedditResponse', return_value=None)
 		response = self.app.get('/?subreddits=futuregarage', content_type='text/html')
 		self.assertEqual(response.status_code, 200)
 		self.assertTrue('No Reddit response' in response.data)
+		flock.getRedditResponse.assert_called_once()
 
 	def test_frontpage_subreddits_no_youtube_links(self):
-		flock.getRedditResponse = mock_getRedditResponse_no_youtube_links
+		return_value = {
+			'data' : {
+				'children' : [
+					{
+						'data' : {
+							'url' : 'imgur.com'
+						}
+					}
+				]
+			}
+		}
+
+		flock.getRedditResponse = mock.MagicMock(name='getRedditResponse', return_value=return_value)
+
 		response = self.app.get('/?subreddits=futuregarage', content_type='text/html')
+
 		self.assertEqual(response.status_code, 200)
 		self.assertTrue('No links found' in response.data)
+		flock.getRedditResponse.assert_called_once()
 	
 	def test_frontpage_subreddits_all_links_present(self):
-		flock.getRedditResponse = mock_getRedditResponse_set_response
+		return_value = {
+			'data' : {
+				'children' : [
+					{
+						'data' : {
+							'title' : 'Burial - Untrue (Full Album Mix)',
+							'url' : 'http://www.youtube.com/watch?v=wRpHf4X7FNM'
+						}
+					},
+					{
+						'data' : {
+							'title' : 'Sage The Gemini - Gas Pedal (Motez Edit)',
+							'url' : 'http://www.youtube.com/watch?v=cfLmW-dKtwg'
+						}
+					},
+					{
+						'data' : {
+							'title' : 'Koreless & Jacques Greene - Untitled',
+							'url' : 'http://www.youtube.com/watch?v=AY08MWIGYsk'
+						}
+					}
+				]
+			}
+		}
+		
+		flock.getRedditResponse = mock.MagicMock(name='getRedditResponse', return_value=return_value)
+
 		response = self.app.get('/?subreddits=futuregarage', content_type='text/html')
 		self.assertEqual(response.status_code, 200)
 		self.assertTrue('http://www.youtube.com/watch?v=wRpHf4X7FNM' in response.data)
@@ -81,6 +76,7 @@ class FrontpageTestCase(unittest.TestCase):
 		self.assertTrue('Sage The Gemini - Gas Pedal (Motez Edit)' in response.data)
 		self.assertTrue('http://www.youtube.com/watch?v=AY08MWIGYsk' in response.data)
 		self.assertTrue('Koreless &amp; Jacques Greene - Untitled' in response.data)
+		flock.getRedditResponse.assert_called_once()
 
 class SanitiseURLCase(unittest.TestCase):
 	def test_sanitise_short_youtube_url(self):
@@ -223,30 +219,11 @@ class DuplicatesTestCase(unittest.TestCase):
 		del links[2]
 		self.assertEquals(new_links, links)
 
-""" replace getYouTubeResponse """
-original_getYouTubeResponse = flock.getYouTubeResponse
-
-def mock_getYouTubeResponse_no_reponse(links):
-	return None
-
-def mock_getYouTubeResponse_fixed_response(links):
-	response_object = {
-		'items' : [
-			{
-				'snippet' : {
-					'title' : 'Burial - Untrue (Full Album Mix)'
-				}
-			}
-		]
-	}
-	return response_object
 
 class GetLinkTitlesTestCase(unittest.TestCase):
-	def tearDown(self):
-		flock.getYouTubeResponse = original_getYouTubeResponse
-
 	def test_get_link_titles_no_response(self):
-		flock.getYouTubeResponse = mock_getYouTubeResponse_no_reponse
+		flock.getYouTubeResponse = mock.MagicMock(name='getYouTubeResponse', return_value=None)
+
 		links = [
 			{
 				'url' : 'http://www.youtube.com/watch?v=wRpHf4X7FNM',
@@ -254,17 +231,31 @@ class GetLinkTitlesTestCase(unittest.TestCase):
 			}
 		]
 		new_links = flock.getLinkTitles(links)
+
 		self.assertEquals(new_links[0]['video_title'], links[0]['title'])
+		flock.getYouTubeResponse.assert_called_once()
 
 	def test_get_link_titles_response(self):
-		flock.getYouTubeResponse = mock_getYouTubeResponse_fixed_response
+		return_value = {
+			'items' : [
+				{
+					'snippet' : {
+						'title' : 'Burial - Untrue (Full Album Mix)'
+					}
+				}
+			]
+		}
+		flock.getYouTubeResponse = mock.MagicMock(name='getYouTubeResponse', return_value=return_value)
+
 		links = [
 			{
 				'url' : 'http://www.youtube.com/watch?v=wRpHf4X7FNM'
 			}
 		]
 		new_links = flock.getLinkTitles(links)
+
 		self.assertEquals(new_links[0]['video_title'], 'Burial - Untrue (Full Album Mix)')
+		flock.getYouTubeResponse.assert_called_once()
 
 if __name__ == '__main__':
 	unittest.main()
